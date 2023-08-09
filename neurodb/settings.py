12fa4c5a0c2563af15c2eadbf5adf53f36d25314
NMunro <neilmunro@gmail.com>
2023-08-09 19:23:06 +0100

    cleaned up allowed hosts

--- neurodb/settings.py
+++ neurodb/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
