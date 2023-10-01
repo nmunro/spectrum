39a1842712a424ee759507dc0e67c85e8d6b99dd
NMunro <neilmunro@gmail.com>
2023-10-01 09:12:07 +0100

    Fixed a bug with logging into the admin dashboard

--- info/templatetags/get_model_type.py
+++ info/templatetags/get_model_type.py
@@ -4,5 +4,4 @@ register = template.Library()
 
 @register.filter(name="get_model_type")
 def get_model_type(obj):
-    print("Got here")
     return obj.__class__.__name__
--- neurodb/settings.py
+++ neurodb/settings.py
@@ -136,7 +136,7 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_REDIRECT_URL = 'dashboard_organisations'
 LOGOUT_REDIRECT_URL = '/'
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
