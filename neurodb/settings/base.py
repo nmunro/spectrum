9b662823ee95de734d3a2943a86f807a0a1bad18
NMunro <neilmunro@gmail.com>
2024-02-25 16:01:46 +0000

    Fixed a setting

--- neurodb/settings/base.py
+++ neurodb/settings/base.py
@@ -121,7 +121,7 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL = 'dashboard_organisations'
+LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = '/'
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
