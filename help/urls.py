7d5c774fc01a5edbf0db043ab2380c81c2174802
NMunro <neilmunro@gmail.com>
2024-02-27 23:25:02 +0000

    #31: Setup help routes and views, along with template files (but no content)

--- help/urls.py
+++ help/urls.py
@@ -5,4 +5,27 @@ from . import views
 
 urlpatterns = [
     path("", views.help, name="help"),
+    path("overview/", views.help, name="overview"),
+    path("organisations/", views.help, name="oragnisations"),
+    path("organisations/edit/", views.help, name="organisations_edit"),
+
+    path("events/", views.help, name="events"),
+    path("events/add/", views.help, name="events_add"),
+    path("events/edit/", views.help, name="events_edit"),
+    path("events/delete/", views.help, name="events_delete"),
+
+    path("resources/", views.help, name="resources"),
+    path("resources/add/", views.help, name="resources_add"),
+    path("resources/edit/", views.help, name="resources_edit"),
+    path("resources/delete/", views.help, name="resources_delete"),
+
+    path("locations/", views.help, name="locations"),
+    path("locations/add/", views.help, name="locations_add"),
+    path("locations/edit/", views.help, name="locations_edit"),
+    path("locations/delete/", views.help, name="locations_delete"),
+
+    path("contacts/", views.help, name="contacts"),
+    path("contacts/add/", views.help, name="contacts_add"),
+    path("contacts/edit/", views.help, name="contacts_edit"),
+    path("contacts/delete/", views.help, name="contacts_delete"),
 ]
--- help/views.py
+++ help/views.py
@@ -2,3 +2,57 @@ from django.shortcuts import render
 
 def help(request):
     return render(request, "help/index.html", {})
+
+def organisations(request):
+    return render(request, "help/index.html", {})
+
+def organisations_edit(request):
+    return render(request, "help/index.html", {})
+
+def events(request):
+    return render(request, "help/index.html", {})
+
+def events_add(request):
+    return render(request, "help/index.html", {})
+
+def events_edit(request):
+    return render(request, "help/index.html", {})
+
+def events_delete(request):
+    return render(request, "help/index.html", {})
+
+def resources(request):
+    return render(request, "help/index.html", {})
+
+def resources_add(request):
+    return render(request, "help/index.html", {})
+
+def resources_edit(request):
+    return render(request, "help/index.html", {})
+
+def resources_delete(request):
+    return render(request, "help/index.html", {})
+
+def locations(request):
+    return render(request, "help/index.html", {})
+
+def locations_add(request):
+    return render(request, "help/index.html", {})
+
+def locations_edit(request):
+    return render(request, "help/index.html", {})
+
+def locations_delete(request):
+    return render(request, "help/index.html", {})
+
+def contacts(request):
+    return render(request, "help/index.html", {})
+
+def contacts_add(request):
+    return render(request, "help/index.html", {})
+
+def contacts_edit(request):
+    return render(request, "help/index.html", {})
+
+def contacts_delete(request):
+    return render(request, "help/index.html", {})
