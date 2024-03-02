026e28227398493d0fbe32637f6df289e502bc50
NMunro <neilmunro@gmail.com>
2024-03-02 00:46:50 +0000

    Removed the last instance of neurodb

--- spectrum/urls.py
+++ spectrum/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for neurodb project.
+URL configuration for spectrum project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
