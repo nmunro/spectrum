b30e55f6bdc9f02512f4c250feae443921513c82
NMunro <neilmunro@gmail.com>
2024-04-09 22:34:13 +0100

    #95 Added templates and routes for devs

--- about/urls.py
+++ about/urls.py
@@ -6,4 +6,6 @@ app_name = "about"
 
 urlpatterns = [
     path("", views.AboutView.as_view(), name="about"),
+    path("nmunro/", views.NMunroView.as_view(), name="nmunro"),
+    path("bhosalepriyanka/", views.BhosalePriyanka.as_view(), name="bhosalepriyanka"),
 ]
--- about/views.py
+++ about/views.py
@@ -3,3 +3,9 @@ from django.views.generic import TemplateView
 
 class AboutView(TemplateView):
     template_name = "about/about.html"
+
+class NMunroView(TemplateView):
+    template_name = "about/nmunro.html"
+
+class BhosalePriyanka(TemplateView):
+    template_name = "about/bhosalepriyanka.html"
