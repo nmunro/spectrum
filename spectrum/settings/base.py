3b0404affe929ab640398ce72369e371f86bb66d
NMunro <neilmunro@gmail.com>
2024-09-01 19:44:15 +0100

    #132: Added styling to cookies list

--- spectrum/settings/base.py
+++ spectrum/settings/base.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
+    "cookie_consent.middleware.CleanCookiesMiddleware",
 ]
 
 ROOT_URLCONF = "spectrum.urls"
